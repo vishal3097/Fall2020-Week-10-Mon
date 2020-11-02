ca9ab73df04982575f20f2569f5e2380e75e6d06
umassdgithub <umassdgithub@users.noreply.github.com>
2020-11-02 11:47:05 -0500

    Examples

---
+++ Example 3 (mouse_events)/scatter_plot.js
@@ -0,0 +1,70 @@
+function scatter_plot(X,Y,markersize,
+                      ColorData,
+                      axis_key,
+                      title="",
+                      xLabel="",
+                      yLabel="",
+                      margin = 100)
+{
+    let xScale= d3.scaleLinear().domain(d3.extent(X)).range([0+margin,1000-margin])
+    let yScale= d3.scaleLinear().domain(d3.extent(Y)).range([1000-margin,0 + margin])
+    let colorScale= d3.scaleLinear().domain(d3.extent(ColorData)).range(['steelblue','brown'])
+    let axis = d3.select(`#${axis_key}`)
+
+    axis.selectAll('.markers')
+        .data(X)
+        .enter()
+        .append('g')
+        .attr('transform', function(d,i) {
+            return `translate(${xScale(X[i])}, ${yScale(Y[i])})`})
+        .append('circle')
+        .attr("class",function (d,i){
+                    return `cls_${i}`})
+        .attr("r",markersize)
+        .attr("fill",function (d,i){return colorScale(ColorData[i])})
+        .on("mouseenter",function (){
+            let mouse_selected_element_class=d3.select(this).attr('class')
+            d3.selectAll(`circle`).classed("highlighted",false).attr("r",markersize)
+            d3.selectAll(`.${mouse_selected_element_class}`).transition().duration(1000).ease(d3.easeBounceOut).attr("r",markersize*4)
+                .classed("highlighted",true)
+        })
+        .on("mouseleave",function(){
+            d3.selectAll(`circle`).classed("highlighted",false).transition().duration(1000).ease(d3.easeBounceOut).attr("r",markersize)
+        })
+
+
+
+
+
+    // x and y Axis function
+    let x_axis = d3.axisBottom(xScale).ticks(4)
+    let y_axis = d3.axisLeft(yScale).ticks(4)
+    //X Axis
+    axis.append("g").attr("class","axis")
+        .attr("transform", `translate(${0},${1000-margin})`)
+        .call(x_axis)
+    // Y Axis
+    axis.append("g").attr("class","axis")
+        .attr("transform", `translate(${margin},${0})`)
+        .call(y_axis)
+    // Labels
+    axis.append("g").attr("class","label")
+        .attr("transform", `translate(${500},${1000-10})`)
+        .append("text")
+        .attr("class","label")
+        .text(xLabel)
+
+    axis.append("g")
+        .attr("transform", `translate(${35},${500}) rotate(270)`)
+        .append("text")
+        .attr("class","label")
+        .text(yLabel)
+    // Title
+    axis.append('text')
+        .attr('x',500)
+        .attr('y',80)
+        .attr("text-anchor","middle")
+        .text(title)
+        .attr("class","title")
+
+}
---
+++ Exercise/Readme.md
@@ -0,0 +1,8 @@
+## MultiView and Single Item Select
+
+1. Make a new HTML file and add 3 SVG elements to it
+2. Load the "data/palmer_penguin_species.tsv"
+3. Use the example 2 (scatter_plot) and add 3 plots such that each plot represents:<br>
+a. One of species only.<br>
+b. X Axis = culmen_length_mm, Y Axis = culmen_depth_mm	and radius flipper_length_mm<br>
+c. Color scale it by the island column.
